c9d2c9db05a3085bcdc85b049b8a1ceacb1891fd
Anthony David <anthony.david34@sfr.fr>
2024-07-25 14:24:25 +0200

    Navbar & MenuBar

---
+++ src/app/(main)/SessionProvider.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { Session, User } from "lucia";
+import React, { createContext, useContext } from "react";
+
+interface SessionContext {
+  user: User;
+  session: Session;
+}
+
+const SessionContext = createContext<SessionContext | null>(null);
+
+export default function SessionProvider({
+  children,
+  value,
+}: React.PropsWithChildren<{ value: SessionContext }>) {
+  return (
+    <SessionContext.Provider value={value}>{children}</SessionContext.Provider>
+  );
+}
+
+export function useSession() {
+  const session = useContext(SessionContext);
+  if (!session) {
+    throw new Error("useSession must be used within a SessionProvider");
+  }
+  return session;
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -14,7 +15,7 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: {
     template: "%s | bugbook",
-    default: "bugbook",
+    default: "Bugbook",
   },
   description: "Social app with Nextjs 15",
 };
@@ -27,7 +28,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
