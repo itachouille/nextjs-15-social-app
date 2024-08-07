072adcc0fac101ed123d9c45e7b09a667c85d406
Anthony David <anthony.david34@sfr.fr>
2024-08-07 09:21:05 +0200

    Comments feature

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -46,6 +46,7 @@ export function getPostDataInclude(loggedInUserId: string) {
     _count: {
       select: {
         likes: true,
+        comments: true,
       },
     },
     bookmarks: {
@@ -66,6 +67,23 @@ export type PostData = Prisma.PostGetPayload<{
 export interface PostsPage {
   posts: PostData[];
   nextCursor: string | null;
+}
+
+export function getCommentDataInclude(loggedInUserId: string) {
+  return {
+    user: {
+      select: getUserDataSelect(loggedInUserId),
+    },
+  } satisfies Prisma.CommentInclude;
+}
+
+export type CommentData = Prisma.CommentGetPayload<{
+  include: ReturnType<typeof getCommentDataInclude>;
+}>;
+
+export interface CommentsPage {
+  comments: CommentData[];
+  previousCursor: string | null;
 }
 
 export interface FollowerInfo {
